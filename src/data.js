d1c77bb1fb4e953b5bf69493e6afb30b1a419d21
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-28 11:57:36 +0200

    added the modal structure

--- src/data.js
+++ src/data.js
@@ -1,46 +1,46 @@
 import React from "react";
-// import {
-//   FaBehance,
-//   FaFacebook,
-//   FaLinkedin,
-//   FaTwitter,
-//   FaSketch,
-//   FaHome,
-//   FaUserFriends,
-//   FaFolderOpen,
-//   FaCalendarAlt,
-//   FaWpforms,
-// } from 'react-icons/fa';
+import {
+  FaBehance,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaSketch,
+  FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+  FaWpforms,
+} from "react-icons/fa";
 export const links = [
   {
     id: 1,
     url: "/",
     text: "home",
-    // icon: <FaHome />,
+    icon: <FaHome />,
   },
   {
     id: 2,
     url: "/team",
     text: "team",
-    // icon: <FaUserFriends />,
+    icon: <FaUserFriends />,
   },
   {
     id: 3,
     url: "/projects",
     text: "projects",
-    // icon: <FaFolderOpen />,
+    icon: <FaFolderOpen />,
   },
   {
     id: 4,
     url: "/calendar",
     text: "calendar",
-    // icon: <FaCalendarAlt />,
+    icon: <FaCalendarAlt />,
   },
   {
     id: 5,
     url: "/documents",
     text: "documents",
-    // icon: <FaWpforms />,
+    icon: <FaWpforms />,
   },
 ];
 
@@ -48,26 +48,26 @@ export const social = [
   {
     id: 1,
     url: "https://www.twitter.com",
-    // icon: <FaFacebook />,
+    icon: <FaFacebook />,
   },
   {
     id: 2,
     url: "https://www.twitter.com",
-    // icon: <FaTwitter />,
+    icon: <FaTwitter />,
   },
   {
     id: 3,
     url: "https://www.twitter.com",
-    // icon: <FaLinkedin />,
+    icon: <FaLinkedin />,
   },
   {
     id: 4,
     url: "https://www.twitter.com",
-    // icon: <FaBehance />,
+    icon: <FaBehance />,
   },
   {
     id: 5,
     url: "https://www.twitter.com",
-    // icon: <FaSketch />,
+    icon: <FaSketch />,
   },
 ];
