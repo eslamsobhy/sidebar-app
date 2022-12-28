37dbc48ebc273399359dff2ad0114a31bf3da068
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-28 16:50:35 +0200

    implemented a global context wrapping the whole app

--- src/context.js
+++ src/context.js
@@ -1,1 +1,13 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+// make sure to add the children destructuring here to avoid a big mess
+// without it the app's childs didn't appear at all even in the component tree
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
+
+// custom hook
+
+export { AppContext, AppProvider };
